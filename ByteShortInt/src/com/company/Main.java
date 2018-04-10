7c0e386f466313da683f61cbc05186cf66c24d5f
Sweeney <brian.sweeney@ibm.com>
2018-04-10 16:15:02 -0400

    max/min values and readable literals;

---
+++ ByteShortInt/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.briansweeney;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int highestPossible = 2_147_483_647;
+        int lowestPossible = -2_147_483_648;
+    }
+}

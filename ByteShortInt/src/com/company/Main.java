eec67339f3e757715ea7bbef3951fa022e831a3d
Sweeney <brian.sweeney@ibm.com>
2018-04-10 16:20:06 -0400

    data types that deal with whole numbers;

--- ByteShortInt/src/com/company/Main.java
+++ ByteShortInt/src/com/company/Main.java
@@ -3,7 +3,18 @@ package com.briansweeney;
 public class Main {
 
     public static void main(String[] args) {
+        //ints can hold large numbers but are space inefficient - width 32
         int highestPossible = 2_147_483_647;
         int lowestPossible = -2_147_483_648;
+
+        //most efficient - width 8
+        byte myByteValue = -128;
+
+        //middle group - width 16
+        short myShortValue = 32767;
+
+        //always ends with a lower or uppercase L, uppercase preferred since it is more readable
+        //width - 64
+        long myLongValue = -9_223_372_036_854_775_808L;
     }
 }

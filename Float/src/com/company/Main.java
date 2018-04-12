22ce5a432f79fa54eaf6e349e98ed8cbd1bea4e0
Sweeney <brian.sweeney@ibm.com>
2018-04-12 15:17:50 -0400

    dividing ints/floats/and doubles;

--- Float/src/com/company/Main.java
+++ Float/src/com/company/Main.java
@@ -3,13 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    int myIntValue = 5;
+        //dividing an int will return the number with no remainder or decimal
+	    int myIntValue = 5/2;
 	    //floats should be followed by an "f" to denote they are floats
         //we can also cast float values -> float test = {float} 5
-	    float myFloatValue = 5f;
+        //float have 7 decimals of precision
+	    float myFloatValue = 5f/3f;
 	    //we should also end a double with D
         //if we use a decimal but neither an f nor a d, the compiler assumes it is a double
-	    double myDoubleValue = 5d;
+        //doubles have 16 decimals of precision
+        //doubles take up twice the amount of memory -> 64 bits/8bytes vs 32bits/4bytes for floats/ints
+        //doubles are faster and more precise so is generally preferred over floats
+	    double myDoubleValue = 5d/3d;
 
         System.out.println("myIntValue " + myIntValue);
         System.out.println("myFloatValue " + myFloatValue);
